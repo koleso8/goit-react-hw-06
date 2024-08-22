97afe12be7c41413cd4305653b23b749a2df52ac
koleso8 <dev.koleso@gmail.com>
2024-08-22 13:01:42 +0300

    Update ContactForm.module.css

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,6 +1,11 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import {
+  addContactsThunk,
+  deleteContactsThunk,
+  fetchContactsThunk,
+} from './contactsOps';
 
-const initialState = { items: [] };
+const initialState = { items: [], loading: false, error: null };
 
 const slice = createSlice({
   name: 'contacts',
@@ -18,6 +23,53 @@ const slice = createSlice({
         item.id === action.payload.id ? { ...item, ...action.payload } : item
       );
     },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContactsThunk.fulfilled, (state, action) => {
+        state.items = action.payload;
+      })
+      .addCase(addContactsThunk.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+      })
+      .addCase(deleteContactsThunk.fulfilled, (state, action) => {
+        state.items = state.items.filter(item => item.id !== action.payload);
+      })
+
+      .addMatcher(
+        isAnyOf(
+          fetchContactsThunk.fulfilled,
+          addContactsThunk.fulfilled,
+          deleteContactsThunk.fulfilled,
+          fetchContactsThunk.rejected,
+          addContactsThunk.rejected,
+          deleteContactsThunk.rejected
+        ),
+        state => {
+          state.loading = false;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContactsThunk.rejected,
+          addContactsThunk.rejected,
+          deleteContactsThunk.rejected
+        ),
+        (state, action) => {
+          state.error = action.payload;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContactsThunk.pending,
+          addContactsThunk.pending,
+          deleteContactsThunk.pending
+        ),
+        state => {
+          state.error = null;
+          state.loading = true;
+        }
+      );
   },
 });
 
